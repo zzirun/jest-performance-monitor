af39cbeba1fdb52fc3d61328f51a1c14c38f6cf9
Zhai Zirun <zirun.zhai19@imperial.ac.uk>
2023-06-22 17:53:45 +0100

    Making imports easier from npm package for quip

---
+++ quip.js
@@ -0,0 +1,5 @@
+const RuntimeContext = require("./runtimeContext/runtimeContext.js");
+const AsyncMode = require("./runtimeContext/asyncModes.js");
+const TimeUnit = require("./runtimeContext/timeUnits.js");
+
+module.exports = {RuntimeContext, AsyncMode, TimeUnit};
